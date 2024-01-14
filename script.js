7e92639de82305ce535d508c217d421aaa1da180
KateFedkova <kate.fedkova@gmail.com>
2024-01-14 17:35:31 +0200

    Add rolling dice functionality

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 'use strict';
 
 // Selecting elements
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 const scoreEl0 = document.querySelector('#score--0');
 const scoreEl1 = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const currentEl0 = document.getElementById('current--0');
+
+const scores = [0, 0];
+let curentScore = 0;
+let activePlayer = 0;
 
 scoreEl0.textContent = 0;
 scoreEl1.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `images/dice-${dice}.png`;
+  if (dice !== 1) {
+    curentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      curentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    curentScore = 0;
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
+  }
+});
